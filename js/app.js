01c2df61695159dbc4aa2bfbb9773139b29d3795
Justyna <j.ataman@tlen.pl>
2020-05-07 16:52:49 +0200

    layout done for now, event listeners next

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
+
+
 function op(obj) {
     x=document.getElementById(obj);
-    if(x.style.display === "none") x.style.display = "block";
+    if(x.style.display === "none") {
+        x.style.display = "block";
+
+    }
     else x.style.display = "none"
 }
