8689fbb26bfea99a44e0556c88bd5e213f58775c
Justyna <j.ataman@tlen.pl>
2020-05-14 15:13:33 +0200

    got rid of onclick in html bc its bad

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
+var button = document.getElementById("paa");
 
+button.addEventListener("click", function(whole){ 
+    console.log('dupa');
+    x=document.getElementById("whole");
+    if(x.style.display === "none") {
+        x.style.display = "block";
 
+    }
+    else x.style.display = "none"
+}); 
+
+/*
 function op(obj) {
     x=document.getElementById(obj);
     if(x.style.display === "none") {
@@ -8,3 +19,5 @@ function op(obj) {
     }
     else x.style.display = "none"
 }
+
+*/
