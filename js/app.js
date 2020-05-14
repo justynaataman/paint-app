47ff496fcad272cc846294cd33bc914417708823
Justyna <j.ataman@tlen.pl>
2020-05-14 20:37:27 +0200

    active tool is highlighted now

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-button = document.querySelector("[data-command=button]")
 
-button.addEventListener("click", function(whole){ 
-  
+//import tools from './tools.js';
+
+button = document.querySelector("[title = Pallete]")
+
+button.addEventListener("click", c => { 
     x=document.getElementById("whole");
     if(x.style.display === "none") {
         x.style.display = "block";
@@ -9,4 +11,49 @@ button.addEventListener("click", function(whole){
     }
     else x.style.display = "none"
 }); 
+//thats for colors button 
+
+brush = document.querySelector("[title = Paintbrush]")
+brush.classList.add("clicked");
+brush.addEventListener("click", q => { 
+    console.log("brush");
+    
+   
+}); 
+
+document.querySelectorAll("[data-t = tool]").forEach(
+    item => {
+            item.addEventListener("click", e=> {
+            document.querySelector("[data-t = tool].clicked").classList.toggle("clicked"); //remove highlight if it is there
+            item.classList.add("clicked");
+            console.log(item.getAttribute("title"));
+        });
+    }
+)
+one = document.querySelector("[title = Size]")
+one.classList.add("clicked");
+
+document.querySelectorAll("[data-width]").forEach(
+    item => {
+            item.addEventListener("click", e=> {
+            document.querySelector("[data-width].clicked").classList.toggle("clicked"); //remove highlight if it is there
+            item.classList.add("clicked");
+            console.log(item.getAttribute("title"));
+        });
+    }
+)
+black = document.querySelector("[title = black]")
+black.classList.add("clicked");
+document.querySelectorAll("[data-c]").forEach(
+    item => {
+            item.addEventListener("click", e=> {
+            document.querySelector("[data-c].clicked").classList.toggle("clicked"); //remove highlight if it is there
+            item.classList.add("clicked");
+            console.log(item.getAttribute("title"));
+        });
+    }
+)
+
+
+
 
---
+++ js/tools.js
@@ -0,0 +1,10 @@
+export default class tools{
+
+static brush = 'brush';
+static eraser = 'eraser'; 
+static fill = 'fill'; 
+static square = 'square'; 
+static circle = 'circle';
+
+
+}
