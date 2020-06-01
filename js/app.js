741ec7b9b5d7aee08724c5c3444c7361bf224b82
Justyna <j.ataman@tlen.pl>
2020-06-01 17:54:03 +0200

    simple drawing for now

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-
-//import tools from './tools.js';
-
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext('2d');
+let draw = 0;
+let selected_color; 
 button = document.querySelector("[title = Pallete]")
 
 button.addEventListener("click", c => { 
@@ -15,21 +16,28 @@ button.addEventListener("click", c => {
 
 brush = document.querySelector("[title = Paintbrush]")
 brush.classList.add("clicked");
+/*
 brush.addEventListener("click", q => { 
     console.log("brush");
     
    
-}); 
-
+});
+*/ 
+//TOOLS CLICKED
 document.querySelectorAll("[data-t = tool]").forEach(
     item => {
             item.addEventListener("click", e=> {
             document.querySelector("[data-t = tool].clicked").classList.toggle("clicked"); //remove highlight if it is there
             item.classList.add("clicked");
             console.log(item.getAttribute("title"));
+
+            let selected = item.getAttribute("title");
+            console.log(selected)
         });
     }
 )
+
+//WIDTH CLICKED
 one = document.querySelector("[title = Size]")
 one.classList.add("clicked");
 
@@ -42,6 +50,8 @@ document.querySelectorAll("[data-width]").forEach(
         });
     }
 )
+
+//COLORS CLICKED
 black = document.querySelector("[title = black]")
 black.classList.add("clicked");
 document.querySelectorAll("[data-c]").forEach(
@@ -50,10 +60,44 @@ document.querySelectorAll("[data-c]").forEach(
             document.querySelector("[data-c].clicked").classList.toggle("clicked"); //remove highlight if it is there
             item.classList.add("clicked");
             console.log(item.getAttribute("title"));
+            selected_color = item.getAttribute("title");
+
         });
     }
 )
 
+canvas.addEventListener('mousedown', e => {
+    x = e.offsetX;
+    y = e.offsetY;
+    draw = 1;
+    console.log(x); 
+    console.log(y);
 
+  });
 
+canvas.addEventListener('mousemove', e => {
+    if (draw === 1) {
+      drawLine(context, x, y, e.offsetX, e.offsetY);
+      x = e.offsetX;
+      y = e.offsetY;
+    }
+  });
 
+  window.addEventListener('mouseup', e => {
+    if (draw === 1) {
+      drawLine(context, x, y, e.offsetX, e.offsetY);
+      x = 0;
+      y = 0;
+      draw = 0;
+    }
+  });
+
+  function drawLine(context, x1, y1, x2, y2) {
+    context.beginPath();
+    context.strokeStyle = selected_color;
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+  }
